b5854d6b3d55ba17391756c50c78359a1d8a74a3
yangjunjie <305870450@qq.com>
2017-02-15 18:29:43 +0800

    1231312

--- public/src/index/index.js
+++ public/src/index/index.js
@@ -24,7 +24,7 @@ function* helloWorldGenerator(x) {
 	console.log('begin!');
 	console.log('Hello' + (yield 123));
   	yield console.log('hello');
-  	yield x =  x + 2;
+  	yield x = x + 2;
   	console.log('end!');
   	return console.log(x);
 }
@@ -36,5 +36,49 @@ console.log(hw.next().value);	//执行到第一个yield这一行
 hw.next('22');	//执行到第二个yield这一行
 hw.next();	//执行到最后
 // hw.next();
+
+function closest(el, selector) {
+    var matchesSelector = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector;
+
+    while (el) {
+        if (matchesSelector.call(el, selector)) {
+            break;
+        }
+        el = el.parentElement;
+    }
+    return el;
+}
+
+var $indexVm = new Vue({
+    el: '#indexView',
+    data: { 
+    	open: {
+    		state: 0,
+    		data: ['Open touch?', 'Close touch?']
+    	}
+    },
+    methods: {
+    	openTouch: function() {
+    		if(this.open.state) {
+    			this.open.state = 0;
+    		} else {
+    			this.open.state = 1;
+    		}
+    	}
+    }
+});
+
+document.body.addEventListener('touchmove', function(e) {
+	console.log('body');
+	
+	var $el = closest(e.target, '.keyTouch');
+	console.log($el);
+
+	if($el) {
+		return;
+	} else {
+		$indexVm.open.state = 1;
+	}
+});
 
 
